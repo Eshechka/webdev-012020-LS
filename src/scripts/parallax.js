343c206f8e590de65fc509706955ac0c25e96bbe
Lidia <ElidiyaE@yandex.ru>
2020-05-10 21:22:43 +0300

    reworked parallax. Now it works with any layers called parallax__layer

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -3,16 +3,19 @@ class Parallax {
     constructor (element) {
         this.section = element;
 
-        this.mountain = this.section.querySelector('.parallax__layer_mountain');
-        this.baloon = this.section.querySelector('.parallax__layer_baloon');
-        this.cloud = this.section.querySelector('.parallax__layer_cloud');
-        this.man = this.section.querySelector('.parallax__layer_man');
-        this.mountain1 = this.section.querySelector('.parallax__layer_mountain1');
-        this.white = this.section.querySelector('.parallax__layer_white');
+        let layersParallax = new Array();
 
+        const layers = this.section.querySelectorAll('.parallax__layer');
+        for (let layer of layers) {          
+            layersParallax.push(
+                {
+                    layer: layer, 
+                    speed: parseInt(layer.dataset.speed) || 1000
+                });
+          }
 
         this.move = function (block, windowScroll, strafeAmount) {
-            let strafe = windowScroll / -strafeAmount + '%';
+            let strafe = -(windowScroll / strafeAmount) + '%';
             let transformString = 'translate3d(0, '+ strafe +', 0)';
             let style = block.style;   
             
@@ -20,12 +23,9 @@ class Parallax {
         }
 
         this.init = function (wScroll) {
-            this.move(this.mountain, wScroll, 55);
-            this.move(this.baloon, wScroll, 8);
-            this.move(this.cloud, wScroll, 10);
-            this.move(this.man, wScroll, 40);
-            this.move(this.mountain1, wScroll, 40);
-            this.move(this.white, wScroll, 100);
+            for (let layer of layersParallax) {
+                this.move(layer.layer, wScroll, layer.speed);
+              }
         }
     }
     
