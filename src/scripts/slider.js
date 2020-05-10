169857cd7e9dae62620dce8ab0b4fe9b4eec68f2
Lidia <ElidiyaE@yandex.ru>
2020-05-10 13:40:55 +0300

    some bug with click event

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,41 +1,61 @@
 import Vue from 'vue';
 
-const arrows = {
-  template: '#arrows',
-};
-const tagslist = {
-  template: '#slider-tags',
-};
-const miniatures = {
-  template: '#slider-miniatures',
-  props: ['dataSlider'],
-};
-const preview = {
-  template: '#slider-preview',
-  components: { miniatures, arrows },
-  props: ['currentSlide','dataSlider'],
-};
-const info = {
-  template: '#slider-decs',
-  components: { tagslist },
-  props: ['currentSlide'],
-};
+console.log('start');
+
+// const arrows = {
+//   template: '#arrows',
+// };
+
+// const tagslist = {
+//   template: '#slider-tags',
+// };
+
+// const miniatures = {
+//   template: '#slider-miniatures',
+//   props: ['dataSlider'],
+// };
+
+// const preview = {
+//   template: '#slider-preview',
+//   components: { miniatures, arrows },
+//   props: ['currentSlide','dataSlider'],
+//   methods: {
+//     handleSlide(direction) {
+//       console.log('arrow click in preview');
+//     },
+//   },
+// };
+// const info = {
+//   template: '#slider-decs',
+//   components: { tagslist },
+//   props: ['currentSlide'],
+// };
 
 
 new Vue ({
     el: '#slider-component',
     template: '#slider',
 
-    components: { preview, info }, 
+    // components: { preview, info }, 
     
     data () {
       return {
         dataSlider: [], 
-        currentSlide: {}
+        currentIndex: 1,
       }
     },
 
+    computed: {
+      currentSlide() {
+        return this.dataSlider[this.currentIndex];
+      }
+      
+    },
+
     methods: {
+      somemethod() {
+        console.log('KUKU');
+      },
       makeArrRequiredImages(array) {
         return array.map(item => {
           const requiredImg = require(`../images/content/${item.photo}`)
@@ -43,13 +63,16 @@ new Vue ({
 
           return item;
         })
-      }
+      },
     },
 
     created () {
-        const data = require('../data/slider.json');
-        this.dataSlider = this.makeArrRequiredImages(data);
-        this.currentSlide = this.dataSlider[0];
+      const data = require('../data/slider.json');
+      this.dataSlider = this.makeArrRequiredImages(data);
+      console.log('created');
+    },
+    mounted () {
+        console.log('mounted');
       }
 
     });
